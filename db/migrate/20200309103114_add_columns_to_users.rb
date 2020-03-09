587ec2d5fc610adf9726def827efb638e15a3aca
Will Price <pricew86@gmail.com>
2020-03-09 18:56:00 +0800

    added user_category model

--- db/migrate/20200309103114_add_columns_to_users.rb
+++ db/migrate/20200309103114_add_columns_to_users.rb
@@ -1,6 +1,5 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :email, :string
     add_column :users, :user_name, :string
     add_column :users, :address, :string
     add_column :users, :phone_number, :string
--- db/migrate/20200309104230_add_columns_to_drivers.rb
+++ db/migrate/20200309104230_add_columns_to_drivers.rb
@@ -1,6 +1,5 @@
 class AddColumnsToDrivers < ActiveRecord::Migration[5.2]
   def change
-    add_column :drivers, :email, :string
     add_column :drivers, :first_name, :string
     add_column :drivers, :last_name, :string
   end
