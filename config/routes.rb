a09a5dff41e59bc1c6cf97524d6f887060327097
Will Price <pricew86@gmail.com>
2020-03-10 11:34:43 +0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,25 @@ Rails.application.routes.draw do
   devise_for :drivers
   devise_for :users
   root to: 'pages#home'
+  
+  resources :pickups, except: [:show]
+  resources :rewards, only: [:index]
+
+  get '/onboarding', to: 'pages#onboarding', as: :onboarding
+  # ROUTES FOR 2 DASHBOARDS
+  get '/dashboard', to: 'dashboards_controller#user_dashboard', as: :user_dashboard
+  get '/driver/dashboard', to: 'dashboards_controller#driver_dashboard', as: :driver_dashboard
+  # MAP FOR DRIVER TO SEE ALL PICKUPS
+  get '/driver/map', to: 'pages#map', as: :map
+  # ROUTES FOR MANAGING USER REWARD PREFERENCE CATEGORIES
+  get '/preferences/manage', to: 'user_categories#new', as: :manage_preferences
+  post '/preferences', to: 'user_categories#create'
+  # REDEEM CODE (pseudo 'new' action) -> CREATE USER_REWARD
+  get '/rewards/:id/redeem', to: 'rewards#redeem', as: :redeem_reward
+  post '/rewards/:id', to: 'user_rewards#create'
+  # FOR DRIVER TO REVIEW A PICKUP
+  # TODO: Trigger callback function on USER Model
+  get '/pickups/:id/review', to: 'pickups#review', as: :review_pickup
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_100501) do
+ActiveRecord::Schema.define(version: 2020_03_09_105923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "badges", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "points"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "drivers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +37,64 @@ ActiveRecord::Schema.define(version: 2020_03_09_100501) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_drivers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_drivers_on_reset_password_token", unique: true
+  end
+
+  create_table "pickups", force: :cascade do |t|
+    t.string "status"
+    t.date "date"
+    t.string "time_of_day"
+    t.text "notes"
+    t.boolean "rating"
+    t.text "feedback"
+    t.bigint "user_id"
+    t.bigint "driver_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["driver_id"], name: "index_pickups_on_driver_id"
+    t.index ["user_id"], name: "index_pickups_on_user_id"
+  end
+
+  create_table "rewards", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "point_cost"
+    t.string "redeem_code"
+    t.integer "amount"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_rewards_on_category_id"
+  end
+
+  create_table "user_badges", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "badge_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["badge_id"], name: "index_user_badges_on_badge_id"
+    t.index ["user_id"], name: "index_user_badges_on_user_id"
+  end
+
+  create_table "user_categories", force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_user_categories_on_category_id"
+    t.index ["user_id"], name: "index_user_categories_on_user_id"
+  end
+
+  create_table "user_rewards", force: :cascade do |t|
+    t.bigint "reward_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reward_id"], name: "index_user_rewards_on_reward_id"
+    t.index ["user_id"], name: "index_user_rewards_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,8 +105,27 @@ ActiveRecord::Schema.define(version: 2020_03_09_100501) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name"
+    t.string "address"
+    t.string "phone_number"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "qr_token"
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "points"
+    t.integer "lifetime_points"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pickups", "drivers"
+  add_foreign_key "pickups", "users"
+  add_foreign_key "rewards", "categories"
+  add_foreign_key "user_badges", "badges"
+  add_foreign_key "user_badges", "users"
+  add_foreign_key "user_categories", "categories"
+  add_foreign_key "user_categories", "users"
+  add_foreign_key "user_rewards", "rewards"
+  add_foreign_key "user_rewards", "users"
 end
