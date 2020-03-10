c094e47364504f145a5c98ea698e81c022409bf1
Will Price <pricew86@gmail.com>
2020-03-10 11:57:58 +0800

    routes finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_update :check_badges
+
+
+  private
+
+  def check_badges
+    # TODO write this code.. happens after the driver gives thumbs up or down
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,18 +8,18 @@ Rails.application.routes.draw do
 
   get '/onboarding', to: 'pages#onboarding', as: :onboarding
   # ROUTES FOR 2 DASHBOARDS
-  get '/dashboard', to: 'dashboards_controller#user_dashboard', as: :user_dashboard
-  get '/driver/dashboard', to: 'dashboards_controller#driver_dashboard', as: :driver_dashboard
+  get '/dashboard', to: 'dashboards#user_dashboard', as: :user_dashboard
+  get '/driver/dashboard', to: 'dashboards#driver_dashboard', as: :driver_dashboard
   # MAP FOR DRIVER TO SEE ALL PICKUPS
   get '/driver/map', to: 'pages#map', as: :map
   # ROUTES FOR MANAGING USER REWARD PREFERENCE CATEGORIES
   get '/preferences/manage', to: 'user_categories#new', as: :manage_preferences
-  post '/preferences', to: 'user_categories#create'
+  post '/user_categories', to: 'user_categories#create'
   # REDEEM CODE (pseudo 'new' action) -> CREATE USER_REWARD
   get '/rewards/:id/redeem', to: 'rewards#redeem', as: :redeem_reward
   post '/rewards/:id', to: 'user_rewards#create'
   # FOR DRIVER TO REVIEW A PICKUP
-  # TODO: Trigger callback function on USER Model
+  # TODO: Trigger callback function on USER Model for checking eligibility of badges
   get '/pickups/:id/review', to: 'pickups#review', as: :review_pickup
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
