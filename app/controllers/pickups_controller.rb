0ad79bca9aeff4b73c9b15a4dd6c86d565a19402
Will Price <pricew86@gmail.com>
2020-03-10 14:55:19 +0800

    added pundit policy to pickups

--- app/controllers/pickups_controller.rb
+++ app/controllers/pickups_controller.rb
@@ -1,10 +1,15 @@
 class PickupsController < ApplicationController
+  after_action: :authorize_pickups, except: [:index]
+  before_action: :set_pickup, only: [:edit, :review, :update, :destroy]
+  
   def index
     #for the driver
+    @pickups = policy_scope(Pickup).order(date: :desc)
   end
 
   def new
     #Scheduling page
+
   end
 
   def edit
@@ -27,4 +32,16 @@ class PickupsController < ApplicationController
     # Patch action
     # used on the review page and edit page
   end
+
+  private 
+  
+  def authorize_pickups
+    authorize @pickup
+  end
+
+  def set_pickup
+    @pickup = Pickup.find(params[:id])
+  end
+
+
 end
