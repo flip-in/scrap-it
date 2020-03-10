cbdf177c2a6856dec9c01ad3f20d089123c1f37c
Alex Prather <prathera14@gmail.com>
2020-03-10 19:51:31 +0800

    created the badge images and the seeds, don't forget to bundle install again for faker

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,5 +1,5 @@
 class Badge < ApplicationRecord
-    validates :name, :decription, :points, presence: true
+    validates :name, :description, :points, presence: true
     validates :points, numericality: true
     validates :name, uniqueness: true
 end
