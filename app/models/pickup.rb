34662634dcf15eaf909e8ae62889621350715439
Victor Therache <codingvictor22@gmail.com>
2020-03-10 12:58:46 +0800

    Validations

--- app/models/pickup.rb
+++ app/models/pickup.rb
@@ -1,5 +1,11 @@
 class Pickup < ApplicationRecord
   belongs_to :user
   belongs_to :driver
-  validates :date, :time_of_day, presence: true
+  validates :date, :time_of_day, presence: true, on: :create
+  validate :validate_review, on: :update
+
+  # def validate_review 
+  # if 
+  # end
+
 end
