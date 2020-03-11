e4b3f2244d1b0c8403a76125dd3b28a9a1c61346
Will Price <pricew86@gmail.com>
2020-03-11 13:08:13 +0800

    added past pickups to the dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < ApplicationController
   def user_dashboard
     @user = current_user
-    @next_pickup = @user.pickups.where('date >= ? AND status= ? OR status = ?', Date.today, 'pending', 'confirmed').first
+    @next_pickup = @user.pickups.where('date >= ? AND status = ? OR status = ?', Date.today, 'pending', 'confirmed').order(date: :asc).first
     @previous_pickups = @user.pickups.where('status= ?', 'complete').order(date: :desc).limit(10)
 
     # Scheduling
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+
+  def parseDate(date)
+    # This is unnecessary since I discovered strftime -.-
+    my_date = {}
+    month = { 1=> "JAN",
+              2=> "FEB",
+              3=> "MAR",
+              4=> "APR",
+              5=> "MAY",
+              6=> "JUN",
+              7=> "JUL",
+              8=> "AUG",
+              9=> "SEP",
+              10=> "OCT",
+              11=> "NOV",
+              12=> "DEC"
+            }
+    parsed = date.strftime
+    parsed = parsed.split("-")
+    parsed.collect!{|string| string.to_i}
+    my_date[:year] = parsed[0]
+    my_date[:month] = month[parsed[1]]
+    my_date[:day] = parsed[2]
+    return my_date
+  end
 end
