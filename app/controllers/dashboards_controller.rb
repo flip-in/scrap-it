25f28f90dd3c951d9b522ec3a075c8a6b3091824
Alex Prather <prathera14@gmail.com>
2020-03-12 13:08:13 +0800

    added functionality to filtering rewards

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,8 +4,13 @@ class DashboardsController < ApplicationController
     @next_pickup = @user.pickups.where('date >= ? AND status = ? OR status = ?', Date.today, 'pending', 'confirmed').order(date: :asc).first
     @previous_pickups = @user.pickups.where('status= ?', 'complete').order(date: :desc).limit(10)
     @user_rewards = current_user.rewards.order(point_cost: :asc)
-    @rewards = Reward.where(category_id: current_user.user_categories.pluck(:category_id)).order(point_cost: :asc)
-    @rewards = @rewards.reject { |i| @user_rewards.include? i }
+    if params[:reward_query]
+      filter_rewards = Reward.joins(:category).where("categories.name = ?", params[:reward_query])
+      @rewards = Reward.where(category_id: filter_rewards.pluck(:category_id)).order(point_cost: :asc)
+    else
+      @rewards = Reward.where(category_id: current_user.user_categories.pluck(:category_id)).order(point_cost: :asc)
+      @rewards = @rewards.reject { |i| @user_rewards.include? i }
+    end
     # Scheduling
     # Rewards
     # Impact
