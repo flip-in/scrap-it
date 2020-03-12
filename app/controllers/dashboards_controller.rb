7de066b121bd23d8d274da3df36cca28daaa4222
Will Price <pricew86@gmail.com>
2020-03-12 13:20:20 +0800

    styled fonts and cards more. added the reward card background

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < ApplicationController
   def user_dashboard
     @user = current_user
-    @next_pickup = @user.pickups.where('date >= ? AND status = ? OR status = ?', Date.today, 'pending', 'confirmed').order(date: :asc).first
+    @next_pickup = @user.pickups.where('date >= ? AND (status = ? OR status = ?)', Date.today, 'pending', 'confirmed').order(date: :asc).first
     @previous_pickups = @user.pickups.where('status= ?', 'complete').order(date: :desc).limit(10)
     @user_rewards = current_user.rewards.order(point_cost: :asc)
     @rewards = Reward.where(category_id: current_user.user_categories.pluck(:category_id)).order(point_cost: :asc)
