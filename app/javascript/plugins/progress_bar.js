adc5554e5f27d959cebf701cce872e8bef2f9cb4
Will Price <pricew86@gmail.com>
2020-03-12 23:39:05 +0800

    added the user_points under the bar and style the color

--- app/javascript/plugins/progress_bar.js
+++ app/javascript/plugins/progress_bar.js
@@ -18,7 +18,7 @@ const progressBar = () => {
           transform: "rotate("+ (45+(p*1.8)) +"deg)", // 100%=180° so: ° = % * 1.8
           // 45 is to add the needed rotation to have the green borders at the bottom
         });
-        $user_points.text(p|0);
+        $user_points.text($user_points_int|0);
       }
     });
   });
