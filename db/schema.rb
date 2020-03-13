2faf37c7c731f00968c376f4250f990212db6adf
Will Price <pricew86@gmail.com>
2020-03-13 11:32:19 +0800

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_103641) do
+ActiveRecord::Schema.define(version: 2020_03_13_032910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,7 +114,7 @@ ActiveRecord::Schema.define(version: 2020_03_10_103641) do
     t.float "latitude"
     t.float "longitude"
     t.integer "points"
-    t.integer "lifetime_points"
+    t.integer "lifetime_points", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
