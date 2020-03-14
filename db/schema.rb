6072a0bc73a9d3e074d3d958d621316f0428a1dd
Alex Prather <prathera14@gmail.com>
2020-03-14 09:26:58 +0800

    just getting started on the functionality here

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_041325) do
+ActiveRecord::Schema.define(version: 2020_03_13_065409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,9 +115,6 @@ ActiveRecord::Schema.define(version: 2020_03_13_041325) do
     t.float "longitude"
     t.integer "points", default: 0
     t.integer "lifetime_points", default: 0
-    t.string "name"
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
