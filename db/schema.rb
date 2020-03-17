061075dd8bdb2072dc170695ea5f60103cef97a3
Victor Therache <codingvictor22@gmail.com>
2020-03-17 11:42:59 +0800

    Avatar column name changed to avatar_id + Driver Login is clearer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_015553) do
+ActiveRecord::Schema.define(version: 2020_03_17_033230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,7 +118,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_015553) do
     t.string "name"
     t.string "provider"
     t.string "uid"
-    t.string "avatar"
+    t.string "avatar_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
