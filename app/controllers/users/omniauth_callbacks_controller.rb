10c275a0cd5a296c183863a87537116a78017823
Victor Therache <codingvictor22@gmail.com>
2020-03-17 15:40:20 +0800

    User#dashboard bug fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,9 +8,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user = User.from_omniauth(request.env["omniauth.auth"])
   
       if @user.persisted?
-        sign_in(@user)        
-        redirect_to edit_user_path @user, event: :authentication #this will throw if @user is not activated
-        set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+        sign_in(@user)
+        # raise
+        if @user.categories.empty?        
+          redirect_to onboarding_path #this will throw if @user is not activated
+          set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+        else
+          redirect_to user_dashboard_path
+        end
       else
         session["devise.facebook_data"] = request.env["omniauth.auth"]
         redirect_to new_user_registration_url
