cb95ebb2f3a8c1a3100537cd3dbd3ebb21921c3f
Will Price <pricew86@gmail.com>
2020-03-17 17:10:36 +0800

    added some logic for redirections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: [:devise_controller?, :drivers_controller?]
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  before_action :redirect_if_no_onboarding, unless: [:devise_controller?]
+
+  #  Uncomment when you *really understand* Pundit!
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
 
   def standard_nav
     @nav_bar_partial = "path/to/standard/nav/partial"
@@ -22,6 +30,16 @@ class ApplicationController < ActionController::Base
       else
         '/dashboard'
       end
+    end
+  end
+
+  def redirect_if_no_onboarding
+    if current_user && current_user.first_name.nil?
+      redirect_to edit_registration_path(current_user) unless controller_name == 'users'
+    elsif current_user && current_user.user_categories.empty?
+      redirect_to onboarding_path unless action_name == 'onboarding' || controller_name == 'user_categories'
+    elsif current_user && current_user.user_rewards.empty?
+      redirect_to new_user_reward_path unless controller_name == 'user_rewards'
     end
   end
 
