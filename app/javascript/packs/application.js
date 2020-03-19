b53fb5a54b53c315b09b0a598878c4b9cfe505f6
Will Price <pricew86@gmail.com>
2020-03-19 16:48:41 +0800

    imported mapbox on application.scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,8 @@ import flatpickr from "flatpickr";
 import { progressBar } from "../plugins/progress_bar"
 import { pickDates } from "../plugins/init_flatpickr";
 import { initCamera } from "../plugins/init_camera";
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+
 import { initMapbox } from '../plugins/init_mapbox';
 
 
